422e28986167221beae9696d3ddf716c591260b8
John Fairhurst <johnfairh@gmail.com>
2018-08-16 08:51:06 +0100

    Fix step count edge cases

--- FFDone/Views/HomeViewController.swift
+++ FFDone/Views/HomeViewController.swift
@@ -22,14 +22,28 @@ class HomeViewController: PresentableVC<HomePresenterInterface> {
     }
 
     private func relayout(current: Int, total: Int) {
-        let donePercent = (total == 0) ? 0 : ((current * 100) / total)
+        let stepsToDo: Double
+        let stepsDone: Double
+
+        if total == 0 {
+            stepsToDo = 0
+            stepsDone = 1
+        } else {
+            stepsDone = Double(current)
+            stepsToDo = Double(total - current)
+        }
+
+        let donePercent = Int((stepsDone * 100) / (stepsDone + stepsToDo))
         progressLabel.text = "\(donePercent)%"
 
         pieChartView.clear()
+        let oldAnimDuration = pieChartView.animDuration
+        pieChartView.animDuration = 0
         pieChartView.models = [
-            PieSliceModel(value: Double(current), color: .green),
-            PieSliceModel(value: Double(total - current), color: .red)
+            PieSliceModel(value: stepsDone, color: .green),
+            PieSliceModel(value: stepsToDo, color: .red)
         ]
         pieChartView.slices.forEach { $0.view.selectedOffset = CGFloat(5.0) }
+        pieChartView.animDuration = oldAnimDuration
     }
 }
