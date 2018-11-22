4dd186c4472da88717d9ff73f80550c7b6605efa
John Fairhurst <johnfairh@gmail.com>
2018-11-22 17:14:34 +0000

    Create empty alarm notes during mig

--- FFDone/App/DatabaseMigration.swift
+++ FFDone/App/DatabaseMigration.swift
@@ -23,6 +23,26 @@ class AlarmMigrationPolicy4to5: NSEntityMigrationPolicy {
                                       in mapping: NSEntityMapping,
                                       manager: NSMigrationManager) throws {
         try super.createRelationships(forDestination: dInstance, in: mapping, manager: manager)
-        print( "*** AlarmMigrationPolicy4to5 ***")
+
+        // Manually recreate transient field with junk to avoid disaster!
+        dInstance.setValue("", forKey: #keyPath(Alarm.sectionOrder))
+
+        // Create empty notes and set them up.  Core Data automatically takes
+        // care of setting the reverse pointers, even at this level.
+        let aNote = createNote(context: manager.destinationContext)
+        dInstance.setValue(aNote, forKey: #keyPath(Alarm.activeNote))
+        let dNote = createNote(context: manager.destinationContext)
+        dInstance.setValue(dNote, forKey: #keyPath(Alarm.defaultNote))
+    }
+
+    private func createNote(context: NSManagedObjectContext) -> NSManagedObject {
+        let note = NSEntityDescription.insertNewObject(forEntityName: "Note", into: context)
+        note.setValue("", forKey: #keyPath(Note.text))
+
+        // confused.com - why am I forced to use NSDate here but a TimeInterval in main code??
+        let now = Date()
+        note.setValue(now, forKey: #keyPath(Note.cdCreationDate))
+        note.setValue(Note.dateToDayStamp(date: now), forKey: #keyPath(Note.dayStamp))
+        return note
     }
 }
